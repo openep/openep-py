23599d0e6004d7ecc78720e5897c31ef9b4993aa
jayrajan <jerin.rajan@kcl.ac.uk>
2021-03-31 12:39:29 +0100

    feat: Trisurf plot with colorbar

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import scipy.io as sio
 import numpy as np
 import pandas as pd
 import trimesh
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+from matplotlib import cm
 
 # relative path to the sample dataset
 # Main MAT File - userdata
@@ -29,24 +32,53 @@ main_file = sio.loadmat(file_path)
 # Vertices/Points
 # data_tri_X = pd.DataFrame(main_file['userdata']['surface_t[riRep_X'])
 data_tri_X = main_file['userdata']['surface_triRep_X'][0][0]
-for item in data_tri_X:
-    data_tri_X_lst.append(item.tolist())
+
+# for item in data_tri_X:
+#     data_tri_X_lst.append(item.tolist())
 
 print('size_of_X:',len(data_tri_X_lst))
 # Triangulation/faces
 # data_tri_T = pd.DataFrame(main_file['userdata']['surface_triRep_Triangulation'][0][0])
 data_tri_T = main_file['userdata']['surface_triRep_Triangulation'][0][0]
 
-for item in data_tri_T:
-    data_tri_T_lst.append(item.tolist())
-
-print('size_of_X:',len(data_tri_T_lst))
+# for item in data_tri_T:
+#     data_tri_T_lst.append(item.tolist())
+#
+print('size_of_T:',len(data_tri_T_lst))
 
 # Convert to a Trimesh object
-mesh = trimesh.Trimesh(vertices=data_tri_X_lst,faces=data_tri_T_lst)
+# mesh = trimesh.Trimesh(vertices=data_tri_X_lst,faces=data_tri_T_lst)
+# mesh = trimesh.Trimesh(vertices=[[0, 0, 0], [0, 0, 1], [0, 1, 0]],faces=[[0, 1, 2]])
+# print(dir(mesh))
+
+x = data_tri_X[:,0]
+y = data_tri_X[:,1]
+z = data_tri_X[:,2]
+t = data_tri_T
+
+print('x:',x)
+print('y:',y)
+print('z:',z)
+print('t',t)
+# print(data_tri_X[:,0])
+# triang = tri.Triangulation(x,y)
+
+# PLot the Mesh
+# Create a new fig window
+
+#
+# ax = plt.figure().add_subplot(projection='3d')
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
 
 
 
+
+p = ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True,cmap=cm.coolwarm)
+ax.set_title('OpenEP TriRep Anatomy Data')
+fig.colorbar(p,ax=ax)
+plt.show()
 
 
 
@@ -73,3 +105,29 @@ mesh = trimesh.Trimesh(vertices=data_tri_X_lst,faces=data_tri_T_lst)
 def draw_map(
         userdata, *arg):
     pass
+
+
+
+
+
+# fig1, ax1 = plt.subplots()
+# ax1.set_aspect('equal')
+# ax1.triplot(triang, 'bo-', lw=1)
+# ax1.set_title('2-D plot')
+
+# fig = plt.figure(figsize=plt.figaspect(0.5))
+# ax = fig.add_subplot(1, 2, 1, projection='3d')
+# ax.plot_trisurf(x, y, z, triangles=tri, cmap=plt.cm.Spectral)
+# ax.set_zlim(-1, 1)
+
+# fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+# X, Y = np.meshgrid(x, y)
+# Z = z
+
+# # Plot the surface.
+# surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+#
+# # Add a color bar which maps values to colors.
+# fig.colorbar(surf, shrink=0.5, aspect=5)
+#
+# plt.show()
