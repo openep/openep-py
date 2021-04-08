f16a69611b0f0c33619846b2c0f2107d7382563b
jayrajan <jerin.rajan@kcl.ac.uk>
2021-04-08 16:11:46 +0100

    feat: replacing the Nan values in voltage with 0, normalising the colorbar with min and max value of voltage

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
-
+import matplotlib as mp
 # Declarations
 file_path = '../openep/openep-examples-main/openep_dataset_1.mat'
 data_tri_X_lst = list()
@@ -27,29 +27,26 @@ data_tri_T = main_file['userdata']['surface_triRep_Triangulation'][0][0]
 # Resolving indexing issue
 t = data_tri_T-1
 
-# ColorShell
-# coloring the shell by the voltage value rather than z-axis height
-data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
-# Get the voltage field data - Second column in act_bip
-print('voltage (act_bip):\n', data_act_bip[:,1])
-print('length of data:', len(data_act_bip))
-print('x values:\n',x)
 
+data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
 voltage_data = data_act_bip[:,1]
-print(type(voltage_data))
+print('voltage data raw',voltage_data)
 # indexRemove = ~np.isnan(voltage_data)
 # print(indexRemove)
 
-# Checking for NaN values in voltage data
-# lambda voltage_data: ' ' if np.isnan(voltage_data) else voltage_data
-# print('voltage_data:\n', voltage_data)
-
-# voltage_new = lambda x: '' if np.isnan(x) else x
-voltage_new = np.asarray(map(lambda x:'' if np.isnan(x) else x, voltage_data))
-print(type(voltage_new))
-# print('index with Nan values:\n',indexRemove)
+# Checking for NaN values and replacing with 0 in voltage data
+voltage_new = np.asarray(list(map(lambda x:0 if np.isnan(x) else x, voltage_data)))
 
 
+# ColorShell
+# coloring the shell by the voltage value rather than z-axis height
+
+min_volt = min(voltage_new)
+max_volt = max(voltage_new)
+print('minimum:',min_volt)
+print('maximum:',max_volt)
+
+norm = mp.colors.Normalize(vmin=min_volt, vmax=max_volt)
 
 # Plot the Mesh
 # Create a new fig window
@@ -57,14 +54,18 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 
 # - Tri-Surface Plot with a rainbow color map
-surf1 = ax1.plot_trisurf(x,y,z, triangles=t, linewidth=0.2, edgecolor=None,antialiased=False,cmap=plt.cm.hsv)
+surf1 = ax1.plot_trisurf(x,y,z, triangles=t, linewidth=0.2, edgecolor=None,antialiased=True,cmap=plt.cm.hsv)
+surf1.set_edgecolor([0,0,0,1])
 ax1.set_title('OpenEP TriRep Anatomy Data')
 
 # Disable axis
 plt.axis('off')
 
+
 # Colour Pallette, Position Left
-cb = plt.colorbar(surf1,ax=[ax1],location='left',label='Voltage (mV)')
+# cb = plt.colorbar(surf1,ax=[ax1],location='left',label='Voltage (mV)')
+cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=plt.cm.hsv),ax=[ax1],location='left',label='Voltage (mV)')
+# cl.LightSource(azdeg=-60,altdeg=30)
 
 # Show plot
 plt.show()
