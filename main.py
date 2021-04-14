3f167963578344d4ed2a70365d6dee8a613b6338
jayrajan <jerin.rajan@kcl.ac.uk>
2021-04-14 10:53:18 +0100

    feat: coloring the shell with the voltage value

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mp
+import sklearn.preprocessing as sk
 # Declarations
 file_path = '../openep/openep-examples-main/openep_dataset_1.mat'
 data_tri_X_lst = list()
@@ -26,6 +27,7 @@ z = data_tri_X[:,2]
 data_tri_T = main_file['userdata']['surface_triRep_Triangulation'][0][0]
 # Resolving indexing issue
 t = data_tri_T-1
+print(t.shape)
 
 
 data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
@@ -38,33 +40,63 @@ print('voltage data raw',voltage_data)
 voltage_new = np.asarray(list(map(lambda x:0 if np.isnan(x) else x, voltage_data)))
 
 
+
 # ColorShell
 # coloring the shell by the voltage value rather than z-axis height
-
 min_volt = min(voltage_new)
 max_volt = max(voltage_new)
-print('minimum:',min_volt)
-print('maximum:',max_volt)
+print('minimum Voltage value:',min_volt)
+print('maximum voltage value:',max_volt)
 
+
+# scaling the z - values to the voltage values
+# z_scaled = np.asarray(list(map(lambda x: x-)))
+
+
+# normalisation
 norm = mp.colors.Normalize(vmin=min_volt, vmax=max_volt)
 
 # Plot the Mesh
 # Create a new fig window
 fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')
+# ax1 = fig.add_subplot(111, projection='3d')
+ax1 = fig.gca(projection='3d')
+# my_col = plt.cm.jet(voltage_new)
+
+# defining a custom color scalar field
+# if only nodal values are known, then the signal need to be averaged by the triangles
+color = np.mean(voltage_new[t], axis=1)
+
+
+print('colorshape:',color.shape )
+print('voltage\n',voltage_new.shape)
+print('voltage_new[t]\n',voltage_new[t])
+print('t\n',t)
+print('color\n',color)
+
+
+# cmap = plt.get_cmap('jet')
+# triang = mp.tri.Triangulation(x,y,t)
+# surf = ax1.plot_trisurf(triang,z,antialiased=True,cmap=plt.cm.jet)
+# surf.set_array(color)
+
 
 # - Tri-Surface Plot with a rainbow color map
-surf1 = ax1.plot_trisurf(x,y,z, triangles=t, linewidth=0.2, edgecolor=None,antialiased=True,cmap=plt.cm.hsv)
-surf1.set_edgecolor([0,0,0,1])
+surf1 = ax1.plot_trisurf(x,y,z,triangles=t,linewidth=0.2, edgecolor=[0,0,0,0],antialiased=True,cmap=plt.cm.jet)
+# surf1 = ax1.plot_surface(x,y,z,traingles=t,linewidth=0.2,antialiased=True,cmap=plt.cm.jet)
+surf1.set_array(color)
+
+
+# surf1.set_edgecolor([0,0,0,1])
+
 ax1.set_title('OpenEP TriRep Anatomy Data')
 
-# Disable axis
 plt.axis('off')
 
 
 # Colour Pallette, Position Left
 # cb = plt.colorbar(surf1,ax=[ax1],location='left',label='Voltage (mV)')
-cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=plt.cm.hsv),ax=[ax1],location='left',label='Voltage (mV)')
+cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=plt.cm.jet),ax=[ax1],location='left',label='Voltage (mV)')
 # cl.LightSource(azdeg=-60,altdeg=30)
 
 # Show plot
