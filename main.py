a293a326e5db5a0cceadbb28e6881542fd34b2d3
jayrajan <jerin.rajan@kcl.ac.uk>
2021-04-27 18:53:57 +0100

    feat: voltage mapping display - drawing the free boundary

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ fig = plt.figure()
 # first plot
 # get the current 3d axis on the current fig
 # ax1 = fig.gca(projection='3d')
-ax1 = fig.add_subplot(1,2,1, projection='3d')
+ax1 = fig.add_subplot(1,1,1, projection='3d')
 
 
 # defining a custom color scalar field
@@ -112,14 +112,31 @@ print('x-y-z points\n',points.shape)
 
 # Trimesh object
 mesh = tm.Trimesh(vertices=points,faces=t)
+# for item in mesh.bounds:
+#     print(item)
+# print(dir(mesh))
+# print(mesh.vertices[:,0])
+# mesh_convex_hull = mesh.convex_hull
+# mesh_convex_hull.show()
 
-for item in mesh.bounds:
-    print(item)
-print(dir(mesh))
+# print('convex hull\n',mesh_convex_hull.shape)
+scene1 = mesh
+scene0 = mesh.outline().vertex_nodes
+# scene0.show()
+# scene1.show()
 
+print('outline type:\n',dir(scene0))
+print('outline paths:\n',scene0)
+
+new_points = mesh.vertices
+print('new_points\n',new_points)
+
+
+mesh_bounds = mesh.bounds
+print(mesh_bounds)
 
 # All edges
-print('total number of edges:\n',mesh.edges.shape)
+print('total number of edges:\n',mesh.edges)
 
 
 
@@ -127,47 +144,70 @@ print('total number of edges:\n',mesh.edges.shape)
 unique_edges = mesh.edges_unique
 print('unique edges of the mesh\n',unique_edges.shape)
 
+# aa = mesh.edges_unique[mesh.faces_unique_edges[:]]
+# print('mesh edges unique - faces unique\n',aa[0])
+
+# facets
+mesh_facets = mesh.facets_boundary
+
+# for item in mesh_facets:
+#     print('facets-list:\n',item)
+# print('mesh facets:\n',type(mesh_facets))
+
+# face adjacency that are shared by the adjacent faces
+face_adjacency_edges = mesh.face_adjacency_edges
+print('face adjacency edges',face_adjacency_edges)
+
 # Identify the unshared edges
 unshared_edges = mesh.face_adjacency_unshared
 print('unshared\n',unshared_edges.shape)
 
-unique_edges_in_unshared = []
-for item in unshared_edges:
-    if item in unique_edges:
-        unique_edges_in_unshared.append(item)
-    else:
-        pass
-unique_edges_in_unshared = np.array(unique_edges_in_unshared)
-print('unique edges in unshared:\n',unique_edges_in_unshared.shape)
+# mesh_edges = mesh.edges
+# print('mesh edges\n',mesh_edges)
+#
 
-# Identify the unique unshared edges
-uniques = {}
-count = 0
-for a,b in unique_edges_in_unshared:
-    if (a,b) not in uniques and (b,a) not in uniques:
-        uniques[(a,b)]=(a,b)
-    else:
-        if (a,b) in uniques:
-            del uniques[(a,b)]
-        else:
-            del uniques[(b,a)]
-# print('uniques\n',uniques)
-
-unique_unshared_edges = np.array(list(uniques.values()))
-print('unique_unshared_edges:\n',unique_unshared_edges.shape)
+#
+#
+#
+# unique_edges_in_unshared = []
+# for item in unshared_edges:
+#     if item in unique_edges:
+#         unique_edges_in_unshared.append(item)
+#     else:
+#         pass
+# unique_edges_in_unshared = np.array(unique_edges_in_unshared)
+# print('unique edges in unshared:\n',unique_edges_in_unshared.shape)
+#
+# # Identify the unique unshared edges
+# uniques = {}
+# count = 0
+# for a,b in unique_edges_in_unshared:
+#     if (a,b) not in uniques and (b,a) not in uniques:
+#         uniques[(a,b)]=(a,b)
+#     else:
+#         if (a,b) in uniques:
+#             del uniques[(a,b)]
+#         else:
+#             del uniques[(b,a)]
+# # print('uniques\n',uniques)
+#
+# unique_unshared_edges = np.array(list(uniques.values()))
+# print('unique_unshared_edges:\n',unique_unshared_edges.shape)
 
 # Get the vertex points for unique unshared edges
 
 
 # extract the ponts/vertices of the unique unshared edges
-for item in unique_unshared_edges:
+
+for item in scene0:
     for i in item:
-        x_values.append(points[i][0])
-        y_values.append(points[i][1])
-        z_values.append(points[i][2])
+        x_values.append(new_points[i][0])
+        y_values.append(new_points[i][1])
+        z_values.append(new_points[i][2])
         # print('unshared_edge_coordinates(x);\n',x_values.append(points[i][0]))
         # print('unshared_edge_coordinates(y);\n',y_values.append(points[i][1]))
         # print('unshared_edge_coordinates(z);\n',z_values.append(points[i][2]))
+# print(np.array(x_values).shape)
 
 # Get the unique edge points
 x_values_array = np.array(x_values).reshape(len(x_values),1)
@@ -201,6 +241,7 @@ print('border_points:\n',unique_unshared_edge_points.shape)
 # tri = Triangulation(x_values_array,y_values_array,z_values_array)
 
 
+
 surf1 = ax1.plot_trisurf(x,
                          y,
                          z,
@@ -216,7 +257,15 @@ surf1.set_array(color)
 # surf1.set_markeredgecolor([0,0,0,1])
 # surf1.set_fc([0.0,1,0])
 ax1.set_title('OpenEP TriRep Anatomy Data')
-plt.axis('on')
+plt.axis('off')
+
+ax1.scatter(unique_unshared_edge_points[:,0],
+            unique_unshared_edge_points[:,1],
+            unique_unshared_edge_points[:,2],
+            c='black',
+            linewidth=1.5,
+            alpha=0.5,
+            marker='.')
 
 # Colour Pallette, Position Left
 cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=newcmp),
@@ -225,14 +274,33 @@ cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=newcmp),
                   label='Voltage (mV)')
 
 
+# ax2 = fig.add_subplot(1,3,2, projection='3d')
+#
+# # ax3 = fig.add_subplot(1,3,3, projection='3d')
+#
+#
+# ax2.set_title('Unique Edge points')
+# ax2.scatter(new_points[:,0],new_points[:,1],new_points[:,2]) # plot the point (2,3,4) on the figure
+# ax2.scatter(unique_unshared_edge_points[:,0],
+#             unique_unshared_edge_points[:,1],
+#             unique_unshared_edge_points[:,2],
+#             c='black',
+#             alpha=1,
+#             marker='.')
 
-ax1 = fig.add_subplot(1,2,2, projection='3d')
-surf2 = ax1.plot_trisurf(xx[0],
-                 yy[0],
-                 zz[0],
-                 triangles=t,
-                 linewidth=0.5)
-surf2.set_edgecolor([0,0,0,1])
+# ax3.set_title('Original Mesh points')
+# plot the point (2,3,4) on the figure
+# ax2.scatter(mesh_bounds[:,0],mesh_bounds[:,1],mesh_bounds[:,2]) # plot the point (2,3,4) on the figure
+
+
+# surf2 = ax2.plot_trisurf(xx[0],
+#                  yy[0],
+#                  zz[0],
+#                  triangles=t,
+#                  linewidth=0.5)
+# surf2.set_edgecolor([0,0,0,1])
+
+
 
 # Show plot
 plt.show()
