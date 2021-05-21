1c75610101ba7d2c96d267856cef6e5440c9c959
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2021-05-21 12:30:34 +0100

    Case additions (#16)
    * Adding mesh routines to cover use cases
    
    * Update case.py, mesh_routines.py, and test_mesh_routines.py
    
    * Python 3.9 issue
    
    * Update io.py

--- openep/io.py
+++ openep/io.py
@@ -2,10 +2,13 @@
 Module containing functions to load OpenEP dataset
 '''
 
+import os
 import numpy as np
 import scipy.io as sio
 import re
 import h5py
+
+from .case import Case
 
 
 def load_mat(filename, exclude_pattern=".*#.*"):
@@ -35,3 +38,34 @@ def load_mat(filename, exclude_pattern=".*#.*"):
         f.visititems(_visitor)  # visit all items in the file and populate dat
 
     return dat
+
+
+def load_case(filename, name=None, exclude_pattern=".*#.*"):
+    """
+    Load a Case object from the given file. This assumes a number of names for objects found in the file.
+    """
+    dat = load_mat(filename, exclude_pattern)
+
+    if name is None:
+        name = os.path.basename(filename)
+
+    nodes = dat["userdata/surface/triRep/X"].T
+    inds = dat["userdata/surface/triRep/Triangulation"].T - 1
+    act, bip = dat["userdata/surface/act_bip"]
+    uni, imp, frc = dat["userdata/surface/uni_imp_frc"]
+
+    fields = dict(act=act, bip=bip, uni=uni, imp=imp, frc=frc)
+    electric = {}
+    rf = {}
+    other_data = {}
+
+    for k, v in dat.items():
+        _, section, *objname = k.split("/", 2)
+
+        if objname and section in ("electric", "rf"):
+            dobj = rf if section == "rf" else electric
+            dobj[objname[0]] = v
+        else:
+            other_data[k] = v
+
+    return Case(name, nodes, inds, fields, electric, rf, other_data)
