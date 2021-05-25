95af0322b4ddbaba52f5fba42c0793c9d45cb52a
jayrajan <jerin.rajan@kcl.ac.uk>
2021-05-25 16:37:53 +0100

    fix - color array

--- tests/main.py
+++ tests/main.py
@@ -8,11 +8,11 @@ import matplotlib as mp
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap, LightSource
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.interpolate import make_interp_spline, BSpline
-# from scipy.special import jn
+from scipy.interpolate import make_interp_spline, BSpline, griddata, interp1d, NearestNDInterpolator
+from matplotlib.cm import jet_r
 
-# Declarations
-file_path = '../openep-py/tests/data/openep_dataset_1.mat'
+# DEFINITIONS
+file_path = '../openep_py/tests/data/openep_dataset_1.mat'
 data_tri_X_lst = []
 data_tri_T_lst = []
 x_values = []
@@ -20,6 +20,17 @@ y_values = []
 z_values = []
 # voltage threshold to split the colorbar by solid and JET
 volt_threshold = 2
+below_color=(0, 0, 0, 255)
+above_color=(255, 0, 255, 255)
+nan_color=(50/255, 50/255, 50/255, 255/255)
+random_color = (6, 202, 255, 0.9)
+color_map=jet_r
+minval=0,
+maxval=1,
+
+color1 = []
+
+
 # Load the file
 # Main MAT File - userdata
 main_file = sio.loadmat(file_path)
@@ -33,23 +44,68 @@ x = data_tri_X[:,0]
 y = data_tri_X[:,1]
 z = data_tri_X[:,2]
 
+x_new = np.reshape(a=x,newshape=(len(x),1))
+y_new = np.reshape(a=y,newshape=(len(y),1))
+z_new = np.reshape(a=z,newshape=(len(z),1))
+#
+print('x-newshape\n',x_new.shape)
+#
+# xx = np.zeros(shape=(len(x),1),dtype=np.float64) + x_new
+# yy = np.zeros(shape=(len(y),1),dtype=np.float64) + y_new
+# zz = np.zeros(shape=(len(z),1),dtype=np.float64) + z_new
+#
+# print('xx\n',xx.shape)
+#
+# print(xx.max(),xx.min())
+# print(yy.max(),yy.min())
+# print(zz.max(),zz.min())
+#
+# # AXIS vectorspoints, values, (grid_x, grid_y
+# # xx = np.linspace(int(x.min()),int(x.max()),len(x),dtype='uint8')
+# # yy = np.linspace(int(y.min()),int(y.max()),len(y),dtype='uint8')
+# # zz = np.linspace(int(z.min()),int(z.max()),len(z),dtype='uint8')
+# #
+# #
+# # print('xx-shape\n',xx)
+# # print('yy-shape\n',yy)
+# # print('zz-shape\n',zz)
+#
+# # Meshgrid
+# xxx,yyy = np.meshgrid(xx,yy)
+# print('xxx\n',xxx)
+# print('yyy\n',yyy)
+
+# x=x_new,y=y_new,z=z_new,kind='near')
+
+
+
+
+
+
+
+
 # Triangulation/faces - .T
 data_tri_T = main_file['userdata']['surface_triRep_Triangulation'][0][0]
 # Resolving indexing issue - matlab to python
 t = data_tri_T-1
+print('t\n',t.shape)
 
 # Voltage Data
 data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
 voltage_data = data_act_bip[:,1]
+print('voltag_bip\n',voltage_data.shape)
+
+voltage_new = np.reshape(a=voltage_data,newshape=(len(voltage_data),1))
+print('voltage-new\n',voltage_new.shape)
 
 # Checking for NaN values and replacing with 0 in voltage data
 voltage_new = np.asarray(list(map(lambda x:0 if np.isnan(x) else x, voltage_data)))
 
 # ColorShell
 # coloring the shell by the voltage value rather than z-axis height
-min_volt = min(voltage_new)
-max_volt = max(voltage_new)
-print(min_volt)
+min_volt = min(voltage_data)
+max_volt = max(voltage_data)
+print('min_volt',min_volt)
 
 # normalisation - normalising the voltage - values
 norm = mp.colors.Normalize(vmin=min_volt, vmax=max_volt)
@@ -57,16 +113,81 @@ norm = mp.colors.Normalize(vmin=min_volt, vmax=max_volt)
 # Plot the Mesh
 # Create a new fig window
 fig = plt.figure(constrained_layout=True,
-                 frameon=True,
-                 figsize=(40,20), dpi=80)
+                 frameon=False,
+                 tight_layout=False,
+                 figsize=(40,20),
+                 dpi=150)
 # first plot
 # get the current 3d axis on the current fig
 ax1 = fig.add_subplot(1,1,1, projection='3d')
 
 # defining a custom color scalar field
 # if only nodal values are known, then the signal need to be averaged by the triangles
+
+# color2 = voltage_data[t]
+# print('color-2',color2)
+
+field = voltage_data
+new_field = np.zeros((field.shape[0], 4), np.int32)
+
+
+# creatig the color array
+for idx in range(len(field)):
+    val = field[idx]
+    # print(val)
+
+    if np.isnan(val):
+        color1.append(nan_color)
+    elif(val<minval):
+        color1.append(below_color)
+    elif(val>maxval):
+        color1.append(above_color)
+    else:
+        color1.append(random_color)
+        # num = val-minval
+        # den = maxval-minval
+        # valscaled = num / den
+
+
+
+color1 = np.array(color1)
+print('color1\n',color1.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print('voltage_new',voltage_new.shape)
+print('t',t)
+print('voltage_new[t]',voltage_new[t].shape)
+
+color3d = voltage_new[t]
+print('color3d',color3d.shape)
+
+
 color = np.mean(voltage_new[t], axis=1)
-print('color-arry\n',color.shape)
+print('color',color.shape)
+
+# color_r = np.linspace(color.min(),color.max(),len(color))
+# color_interp = interp1d(color_r)
+# print('color-arry\n',color_r)
 
 # Voltage Threshold - to split the colorbar axis
 # determining the size of the voltage values
@@ -78,7 +199,7 @@ dist_above_threshold = max_volt - volt_threshold
 # color below threshold - size
 size_below_threshold = round((dist_below_threshold/diff_volt)*256)
 # color above threshold - size
-size_above_threshold = 256 - size_below_threshold
+size_above_threshold = int(256 - size_below_threshold)
 
 # Define Colormap - Reverse JET
 cm_jet = plt.cm.get_cmap('jet_r',size_below_threshold)
@@ -92,8 +213,10 @@ gray = np.array([128/255, 128/255, 128/255, 1])
 # Creating new colormap columns
 # Col 1 - Jet
 col1 = cm_jet(np.linspace(0,1,size_below_threshold))
+print('col1\n',col1)
 # Col2 - Magenta
-col2 = np.zeros((size_above_threshold,4)) + magenta
+col2 = np.zeros(shape=(size_above_threshold,4))+magenta
+# col3 = np.ones(size_above_threshold)
 # Combining Col1 + Col2
 final_col = np.concatenate((col1,col2))
 print('final_col\n',final_col.shape)
@@ -106,6 +229,11 @@ print('points\n',points)
 # Trimesh object
 mesh = tm.Trimesh(vertices=points,faces=t)
 # mesh.show()
+# mesh.visual.base.Visuals(vertex_colors=final_col)
+# mesh.show()
+
+
+
 freeboundary = mesh.outline().to_dict()
 vertex_index = mesh.outline().vertex_nodes
 mesh_outline_entities = mesh.outline().entities
@@ -144,7 +272,7 @@ for item in freeboundary_points:
                                  item[:,1],
                                  item[:,2],
                                  c='black',
-                                 linewidth=2,
+                                 linewidth=1,
                                  zorder=3
                                  )
 
@@ -155,7 +283,7 @@ ls = LightSource(azdeg=315,altdeg=65)
 # Shade data, creating a rgb array
 rgb = ls.shade(final_col, cmap=newcmp)
 print('rgb\n',rgb.shape)
-
+# plt.imshow(rgb)
 
 # Trisurface 3-D Mesh Plot
 surf1 = ax1.plot_trisurf(x,
@@ -164,17 +292,22 @@ surf1 = ax1.plot_trisurf(x,
                          triangles=t,
                          linewidth=0.1,
                          antialiased=True,
+                         # color='gray',
                          cmap=newcmp,
+                         shade=True
                          )
 
 # passing the custom color scalar field to point the voltage values not z-values
 
-print(dir(surf1))
 surf1.set_array(color)
-surf1.set_facecolors(rgb)
+
+# surf1.set_array(color3d[:,0])
+# surf1.set_array(color3d[:,1])
+# surf1.set_array(color3d[:,2])
+
+# surf1.set_facecolors(rgb)
 ax1.set_title('OpenEP TriRep Anatomy Data')
 plt.axis('off')
-
 # Colour Pallette
 cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=newcmp),
                   ax=[ax1],
@@ -182,6 +315,7 @@ cb = plt.colorbar(mp.cm.ScalarMappable(norm=norm, cmap=newcmp),
                   label='Voltage (mV)')
 
 # Show plot
+
 plt.show()
 
 
