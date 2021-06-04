3c6944175432dbb5db380c19a2a3251646f0879d
jayrajan <jerin.rajan@kcl.ac.uk>
2021-06-04 13:18:28 +0100

    feat: def getMappingPointsWithinWOI

---
+++ openep/case_routines.py
@@ -0,0 +1,36 @@
+
+
+def getMappingPointsWithinWoI(mesh_case):
+    '''
+    GETMAPPINGPOINTSWITHINWOI Returns the indices of the mapping points with
+    annotated local activation time within the window of interest
+
+    Args:
+        mesh_case: Case
+
+    Returns:
+        logical array list of valid points; indexes into mesh_case.electric
+    '''
+    
+    # reference annotation
+    referenceAnnot = mesh_case.electric['annotations/referenceAnnot'].T
+
+    # woi
+    woi = mesh_case.electric['annotations/woi'].T
+
+    # Extract the region of the electrogram of interest for each mapping point, n
+    woi = woi + referenceAnnot
+
+    # MapAnnot
+    mapAnnot = mesh_case.electric['annotations/mapAnnot'].T
+
+    iPoint = np.ones(shape=mapAnnot.shape, dtype=np.bool)
+
+    # Remove the data points that are outside the region of interest
+    for indx in range(mapAnnot.size):
+        if (mapAnnot[indx]<woi[indx,0]) or (mapAnnot[indx]>woi[indx,1]):
+            iPoint[indx] = False
+        else:
+            iPoint[indx] = True
+
+    return iPoint
