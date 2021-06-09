450f2acba9f20ce96f49a1f87c62bbfe3d58f6da
jayrajan <jerin.rajan@kcl.ac.uk>
2021-06-09 12:14:00 +0100

    feat: calculate point distance max

--- openep/case_routines.py
+++ openep/case_routines.py
@@ -1,8 +1,9 @@
 
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from sklearn.neighbors import NearestNeighbors, KDTree
 from scipy.interpolate import LinearNDInterpolator as linterp
 from scipy.interpolate import NearestNDInterpolator as nearest
+from scipy.interpolate import Rbf
 
 def getMappingPointsWithinWoI(mesh_case):
     '''
@@ -61,6 +62,18 @@ def distBetweenPoints(A, B):
 
     return d
 
+def calculate_point_distance_max(points, test_points, max_distance):
+    results = []
+
+    dists = []
+
+    for p in test_points:
+        dist = np.linalg.norm(points - p, axis=1)
+        inds = np.argwhere(dist <= max_distance).flatten()
+        results.append(inds)
+        dists.append(dist)
+
+    return results, dists
 
 class LinearNDInterpolatorExt(object):
     ''' 
@@ -80,17 +93,51 @@ class LinearNDInterpolatorExt(object):
             return z
 
 
+def LocalSmoothing(x0,x1,smoothingLength):
+    # print(len(x1))
+    # print(len(x0))
+    f_dash = np.zeros(shape=(len(x1),1),dtype=np.float64)
+    df_dash = np.zeros(shape=(len(x1),3),dtype=np.float64)
+    # [idx, dists] = calculate_point_distance_max(x0,x1,smoothingLength)
+    # for item in idx:
+    #     print(item)
+    # print(np.array(dists).shape)
+    # no_smooth = 
+
+
+
+
+
+    return [f_dash,df_dash]
+
+
+def rbf(x0,d0,x1,rbfConstant):
+    print(x0.shape)
+    print(x1[:,0].shape)
+
+    x = x0[:,0]
+    y = x0[:,1]
+    z = x0[:,2]
+    d = d0
+
+    print(x.shape, y.shape, z.shape, d.shape)
+
+    F = Rbf(x,y,z, d, function='linear', smooth=rbfConstant, mode='N-D')
+    d1 = F(x1[:,0],x1[:,1],x1[:,2])
+
+    return d1
+
+
 class openEpDataInterpolator():
 
     '''
     OPENEPDATAINTERPOLATOR Creates objects for performing spatial
     interpolation for OpenEP data
     '''
-
-    method = 'scatteredinterpolant'
-    def __init__(self,method,distanceThreshold):
+    def __init__(self,method,distanceThreshold,rbfConstant):
         self.method = method
         self.distanceThreshold = distanceThreshold
+        self.rbfConstant = rbfConstant
 
 
     def interpolate(self,x0,d0,x1,*args):
@@ -98,8 +145,24 @@ class openEpDataInterpolator():
         self.d0 = d0
         self.x1 = x1
 
-        F = LinearNDInterpolatorExt(points=(self.x0[:,0],self.x0[:,1],self.x0[:,2]),values=self.d0)
-        d1 = np.array(F(self.x1[:,0],self.x1[:,1],self.x1[:,2])).reshape(self.x1.shape[0],1)
+        print(self.x0.shape)
+        print(self.x1.shape)
+
+        if self.method == 'scatteredinterpolant':
+            F = LinearNDInterpolatorExt(points=(self.x0[:,0],self.x0[:,1],self.x0[:,2]),values=self.d0)
+            d1 = np.array(F(self.x1[:,0],self.x1[:,1],self.x1[:,2])).reshape(self.x1.shape[0],1)
+        
+        elif self.method == 'rbf':
+            F = Rbf(self.x0[:,0],self.x0[:,1],self.x0[:,2],self.d0,function='linear',epsilon=self.rbfConstant)
+            d1 = F(self.x1[:,0],self.x1[:,1],self.x1[:,2])
+
+        elif self.method == 'localSmoothing':
+            pass
+        
+        else:
+            print('Interpolation Method not Found')
+
+
 
         # Workout if there are any points on the surface that are < 0
         d1[d1<0] = 0
