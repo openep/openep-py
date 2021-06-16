46713eb2caf65cba018050c464dc511833904003
jayrajan <jerin.rajan@kcl.ac.uk>
2021-06-16 13:34:08 +0100

    feat: loading the 3d surf plot on the click of button

--- openep/view/view_pyqt.py
+++ openep/view/view_pyqt.py
@@ -6,14 +6,23 @@ GUI code for OpenEp
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
-import scipy.io as sio
-import pyqtgraph as pg
-import numpy as np
-import trimesh as tm
+import pyvistaqt as pyvqt
+
+# import scipy.io as sio
+# import pyqtgraph as pg
+# import numpy as np
+# import trimesh as tm
 
 import sys
 sys.path.append('../openep')
-import case
+
+from openep import io as openep_io
+from openep import case as openep_case
+from openep import mesh_routines as openep_mesh
+from openep import case_routines as case_routines
+from openep import draw_routines as draw
+
+
 
 class App(QWidget):
 
@@ -45,6 +54,7 @@ class App(QWidget):
         vbox.addWidget(mainLabel2)
 
 
+
         # Button 1
         button1 = QPushButton('Load OpenEp Data', self)
         button1.setGeometry(200,150,100,40)
@@ -52,7 +62,7 @@ class App(QWidget):
 
 
         # Button 2
-        button2 = QPushButton('Plot Voltage Map', self)
+        button2 = QPushButton('Plot 3D Mesh', self)
         button2.setGeometry(200,150,100,40)
         button2.clicked.connect(self.on_click2)
 
@@ -63,8 +73,11 @@ class App(QWidget):
         vbox.addRow(hbox)
 
         # Plot
-        vbox.addRow(QLabel('Plot'))
-        vbox.addRow(pg.PlotWidget())
+        self.plotter = pyvqt.BackgroundPlotter()
+        vbox.addRow(QLabel('3-D Plot'))
+        
+
+        # vbox.addRow(pg.PlotWidget())
 
         # create a mesh object,
         # display the mesh
@@ -91,7 +104,7 @@ class App(QWidget):
         self.show()
 
     def on_click(self):
-        print('Loading Data ... ')
+        print('Please wait: Loading Data ... ')
 
         # Loading file from a Dialog Box
         dlg = QFileDialog()
@@ -99,67 +112,13 @@ class App(QWidget):
 
         if dlg.exec_():
             filenames = dlg.selectedFiles()
-            load_data(filenames)
-            # mesh_obj = case.Case()
-            # case_file = case.load_case(filenames[0])
-            #
-            # print(case_file('nodes'))
-
-
-
-
-            print(filenames[0])
-            main_file = sio.loadmat(filenames[0])
-            data_tri_X = main_file['userdata']['surface_triRep_X'][0][0]
-            t = main_file['userdata']['surface_triRep_Triangulation'][0][0] - 1
-            data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
-            voltage_data = data_act_bip[:,1]
-            x = data_tri_X[:,0]
-            y = data_tri_X[:,1]
-            z = data_tri_X[:,2]
-            print('Nodes\n',data_tri_X)
-            print('tri_data\n',t)
-            print('Data Successfully Loaded')
-            self.mesh_obj = tm.Trimesh(vertices=data_tri_X,faces=t)
-            self.voltage_new = np.asarray(list(map(lambda x:0 if np.isnan(x) else x, voltage_data)))
-
-
-
-
+            self.ep_case = openep_io.load_case(filenames[0])
 
 
     def on_click2(self):
-        self.mesh_obj
-        self.voltage_new
+        _ = self.plotter.add_mesh(pv.Sphere())
+        draw.DrawMap(self.ep_case)
 
-        # have an if condition if its none, do not run visualisation or have an
-        # advisory in the GUI.
-
-
-
-        pass
-        # print('Loading Voltage Map ... ')
-        # hour = [1,2,3,4,5,6,7,8,9,10]
-        # temperature = [30,32,34,32,33,31,29,32,35,45]
-        # print('hour\n',hour)
-        # print('temperature\n',temperature)
-        # self.graph.plot(hour, temperature)
-
-    # def load_data(filename):
-    #     main_file = sio.loadmat(filenames[0])
-    #     data_tri_X = main_file['userdata']['surface_triRep_X'][0][0]
-    #     t = main_file['userdata']['surface_triRep_Triangulation'][0][0] - 1
-    #     data_act_bip = main_file['userdata']['surface'][0][0]['act_bip'][0][0]
-    #     voltage_data = data_act_bip[:,1]
-    #     x = data_tri_X[:,0]
-    #     y = data_tri_X[:,1]
-    #     z = data_tri_X[:,2]
-    #     print('Nodes\n',data_tri_X)
-    #     print('tri_data\n',t)
-    #     print('Data Successfully Loaded')
-    #     mesh_obj = tm.Trimesh(vertices=data_tri_X,faces=t)
-    #     voltage_new = np.asarray(list(map(lambda x:0 if np.isnan(x) else x, voltage_data)))
-    #     return data_tri_X, data_tri_T, voltage_new
 
 
 
