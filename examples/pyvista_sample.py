b84380fa3734a1c4c7bd550bdd4c0c59b826eea0
jayrajan <jerin.rajan@kcl.ac.uk>
2021-06-16 13:36:20 +0100

    Initial commit, feat: plotting 3d surface mesh using pyvista, plotting freeboundary edges, surface color based on voltage[bip] values

---
+++ examples/pyvista_sample.py
@@ -0,0 +1,23 @@
+from openep import io as openep_io
+from openep import case as openep_case
+from openep import mesh_routines as openep_mesh
+from openep import case_routines as case_routines
+from openep import draw_routines as draw
+
+import pyvista as pv
+import numpy as np
+from matplotlib.cm import jet, rainbow, jet_r, seismic
+import trimesh as tm
+
+
+filename = '/home/jra21/work/source/repos/opep/examples/data/new_dataset_2.mat'
+np.set_printoptions(suppress=True)
+
+
+ep_case = openep_io.load_case(filename)
+ep_case_mesh = ep_case.create_mesh()
+
+
+draw.DrawMap(ep_case,color=[220/250,220/250,220/250],freeboundary_width=5)
+
+
