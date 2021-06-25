5ff6b8f7d41260a41873aadf98e37e29196981a7
jayrajan <jerin.rajan@kcl.ac.uk>
2021-06-25 18:00:43 +0100

    feat: usecase09 (#38) - implemented, freeboundary points, Trirep of the boundary, length of the perimeter and area of each freeboundary mesh object

--- openep/draw_routines.py
+++ openep/draw_routines.py
@@ -6,44 +6,81 @@ from matplotlib.cm import jet, rainbow, jet_r, seismic
 import trimesh as tm
 
 def getAnatomicalStructures(ep_case, *args):
-        pts = ep_case.nodes
-        tri = ep_case.indices.astype(int)
-        tm_mesh = tm.Trimesh(vertices=pts,faces=tri)
-        freeboundary = tm_mesh.outline().to_dict()
-        mesh_outline_entities = tm_mesh.outline().entities
-        freeboundary_vertex_nodes = []
-        freeboundary_points = []
+    x_values = []
+    y_values = []
+    z_values = []
+
+    tri = []
+    a = []
+    dist = []
+    l = []
+    
+    pts = ep_case.nodes
+    face = ep_case.indices.astype(int)
+        
+    tm_mesh = tm.Trimesh(vertices=pts,faces=face)
+    freeboundary = tm_mesh.outline().to_dict()
+    mesh_outline_entities = tm_mesh.outline().entities
+    freeboundary_vertex_nodes = []
+    freeboundary_points = []
 
 
-        for i in range(len(mesh_outline_entities)):
-            freeboundary_vertex_nodes.append(freeboundary["entities"][i]['points'])
-
-        x_values = []
-        y_values = []
-        z_values = []
-
-        # mesh Vertices
-        trimesh_points = tm_mesh.vertices
-
-        for i in freeboundary_vertex_nodes:
-            x_values.append(trimesh_points[i][:,0])
-            y_values.append(trimesh_points[i][:,1])
-            z_values.append(trimesh_points[i][:,2])
-
-        x_values_array = []
-        y_values_array = []
-        z_values_array = []
+    for i in range(len(mesh_outline_entities)):
+        freeboundary_vertex_nodes.append(freeboundary["entities"][i]['points'])
 
 
-        for i in range(len(mesh_outline_entities)):
-            x_values_array.append(x_values[i].reshape(len(x_values[i]),1))
-            y_values_array.append(y_values[i].reshape(len(y_values[i]),1))
-            z_values_array.append(z_values[i].reshape(len(z_values[i]),1))
-            freeboundary_points.append(np.concatenate((x_values_array[i],
-                                                y_values_array[i],
-                                                z_values_array[i]),
-                                                axis=1))
-        return freeboundary_points
+    # mesh Vertices
+    trimesh_points = tm_mesh.vertices
+
+    for i in freeboundary_vertex_nodes:
+        x_values.append(trimesh_points[i][:,0])
+        y_values.append(trimesh_points[i][:,1])
+        z_values.append(trimesh_points[i][:,2])
+
+    x_values_array = []
+    y_values_array = []
+    z_values_array = []
+
+
+    for i in range(len(mesh_outline_entities)):
+        x_values_array.append(x_values[i].reshape(len(x_values[i]),1))
+        y_values_array.append(y_values[i].reshape(len(y_values[i]),1))
+        z_values_array.append(z_values[i].reshape(len(z_values[i]),1))
+        freeboundary_points.append(np.concatenate((x_values_array[i],
+                                            y_values_array[i],
+                                            z_values_array[i]),
+                                            axis=1))
+
+    freeboundary_points = np.array(freeboundary_points,dtype=object)
+
+
+    for i in range(len(freeboundary_points)):
+        coords = freeboundary_points[i]
+        centre = np.round(np.mean(coords,0),3)
+        centre = centre.reshape(1,len(centre))
+
+        # Create a Trirep of the boundary
+        X = np.vstack((centre,coords))
+        numpts = X.shape[0]
+        A = np.zeros(((numpts-1),1),dtype=np.int64)
+        B = np.array(range(1,numpts)).T
+        B = B.reshape(len(B),1)
+        C = np.array(range(2,numpts)).T
+        C = C.reshape(len(C),1)
+        C = np.vstack((C,1))
+        TRI = np.concatenate((A,B,C),axis=1)
+
+        for j in range(len(coords)-1):
+            dist.append(np.linalg.norm(np.subtract(coords[j+1],coords[j])))
+
+        tri.append(tm.Trimesh(vertices=X,faces=TRI))
+        a.append(round(tri[i].area,4))
+        l.append(round(np.sum(dist),4))
+        print('Periemeter is :',l[i],'| Area is:',a[i])
+
+
+    return freeboundary_points
+       
 
 
 
