2c3403efe4f09902d54050ea1bd1132001aaf155
jayrajan <jerin.rajan@kcl.ac.uk>
2021-08-16 15:09:26 +0100

    minor fix

--- examples/openep_drawroutines.py
+++ examples/openep_drawroutines.py
@@ -16,17 +16,17 @@ filename = '/home/jra21/work/source/repos/opep/examples/data/new_dataset_2.mat'
 ep_case = openep_io.load_case(filename)
 
 # DrawVoltage Map
-# hsurf = draw.draw_map(ep_case,
-#                     volt='bip',
-#                     freeboundary_color='black',
-#                     freeboundary_width=5,
-#                     cmap='jet_r',
-#                     minval=0,
-#                     maxval=2,
-#                     volt_below_color='brown', 
-#                     volt_above_color='magenta', 
-#                     nan_color='gray',
-#                     plot=True)
+hsurf = draw.draw_map(ep_case,
+                    volt='bip',
+                    freeboundary_color='black',
+                    freeboundary_width=5,
+                    cmap='jet_r',
+                    minval=0,
+                    maxval=2,
+                    volt_below_color='brown', 
+                    volt_above_color='magenta', 
+                    nan_color='gray',
+                    plot=True)
 
 # GetAnatomicalStructure and DrawFreeBoundaries on mesh
-hsurf1 = draw.get_anatomical_structures(ep_case,plot=True)
+# hsurf1 = draw.get_anatomical_structures(ep_case,plot=True)
