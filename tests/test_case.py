96a71edcfb5d49f33a1dc588199b6aecca767c92
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-13 17:57:04 +0100

    Update test_case.py to use PyVista

--- tests/test_case.py
+++ tests/test_case.py
@@ -1,16 +1,24 @@
 from unittest import TestCase
-import trimesh
-from openep.case import Case
+from numpy.testing import assert_allclose
 
+import pyvista
+
+from openep.case import Case
+from openep._simple_meshes.simple_meshes import (
+    CUBE, SPHERE, BROKEN_SPHERE
+)
 
 class CaseTests(TestCase):
     def setUp(self) -> None:
-        box = trimesh.creation.box()
-        field = box.vertices[:, 0]
+        cube = pyvista.read(CUBE)
+        
+        self.fakefield = cube.points[:, 0]
+        self.case = Case("Test", cube.points, cube.faces, {"fakefield": self.fakefield}, {}, {})
 
-        self.case = Case("Test", box.vertices, box.faces, {"fakefield": field}, {}, {})
+    def test_fields(self):
+        assert_allclose(self.fakefield, self.case.fields['fakefield'])
 
     def test_mesh_creation(self):
         mesh = self.case.create_mesh()
-
-        self.assertTrue(mesh._kwargs["parent_obj"] is self.case)
+        self.assertIsInstance(mesh, pyvista.PolyData)
+        
