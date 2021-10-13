e8b2e5f7c0982606f3b9e17d583edfe97f880a55
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-13 17:24:24 +0100

    Add simple test systems created using TriMesh

---
+++ openep/_simple_meshes/simple_meshes.py
@@ -0,0 +1,23 @@
+"""
+Simple meshes created for testing openep
+========================================
+
+The meshes were originally created using trimesh.creation
+
+"""
+__all__ = [
+    "CUBE",
+    "SPHERE",
+    "BROKEN_SPHERE"
+]
+
+from pkg_resources import resource_filename
+
+CUBE = resource_filename(__name__,
+                         "PLY/cube.ply")
+
+SPHERE = resource_filename(__name__,
+                         "PLY/sphere.ply")
+
+BROKEN_SPHERE = resource_filename(__name__,
+                         "PLY/broken_sphere.ply")
