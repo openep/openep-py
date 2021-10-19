f579eda2da85e9f2b8ec877263b123e2212aba5e
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-19 19:49:23 +0100

    Add new simple mesh for testing (triangles.ply)

--- openep/_simple_meshes/simple_meshes.py
+++ openep/_simple_meshes/simple_meshes.py
@@ -2,13 +2,17 @@
 Simple meshes created for testing openep
 ========================================
 
-The meshes were originally created using trimesh.creation
+CUBE, SPHERE, and BROKEN_SPHERE were originally created using trimesh.creation
+
+TRIANGLES is a set of fives triangles. The first four are connected such that
+they form a unit square. The fifth is disconnected from the others.
 
 """
 __all__ = [
     "CUBE",
     "SPHERE",
-    "BROKEN_SPHERE"
+    "BROKEN_SPHERE",
+    "TRIANGLES",
 ]
 
 from pkg_resources import resource_filename
@@ -21,3 +25,6 @@ SPHERE = resource_filename(__name__,
 
 BROKEN_SPHERE = resource_filename(__name__,
                                   "PLY/broken_sphere.ply")
+
+TRIANGLES = resource_filename(__name__,
+                             "PLY/triangles.")
