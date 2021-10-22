0e386b468efa06b94f2968b8af72f72e1b9384a8
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-22 18:13:47 +0100

    Move plot_electrograms from case_routines to draw_routines

--- openep/case/__init__.py
+++ openep/case/__init__.py
@@ -4,7 +4,6 @@ from .case_routines import (
     get_mapping_points_within_woi,
     get_woi_times,
     get_electrograms_at_points,
-    plot_electrograms,
     calculate_distance,
     calculate_points_within_distance,
     LinearNDInterpolatorExt,
