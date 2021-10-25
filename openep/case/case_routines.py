09c4e6c88e6563ad0e6f68315922738be06b71d7
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-25 13:11:27 +0100

    Use scipy for interpolation

--- openep/case/case_routines.py
+++ openep/case/case_routines.py
@@ -16,16 +16,10 @@
 # You should have received a copy of the GNU General Public License along
 # with this program (LICENSE.txt).  If not, see <http://www.gnu.org/licenses/>
 
-import sys
-import time
-import operator
-import functools
-import numpy as np
+from dataclasses import dataclass
 
-import scipy.spatial
-from scipy.interpolate import Rbf
-from scipy.interpolate import LinearNDInterpolator as linterp
-from scipy.interpolate import NearestNDInterpolator as nearest
+import numpy as np
+import scipy.interpolate
 
 __all__ = [
     'get_mapping_points_within_woi',
@@ -34,14 +28,7 @@ __all__ = [
     'calculate_voltage_from_electrograms',
     'calculate_distance',
     'calculate_points_within_distance',
-    'LinearNDInterpolatorExt',
-    'LocalSmoothing',
-    'rbfAssemble',
-    'rbfcreate',
-    'rbfinterp',
-    'rbfcheck',
-    'rbf_scipy',
-    'OpenEPDataInterpolator',
+    'Interpolator',
     'get_voltage_electroanatomic',
 ]
 
@@ -299,373 +286,76 @@ def calculate_points_within_distance(origin, destination, max_distance, return_d
     return within_max_distance
 
 
-class LinearNDInterpolatorExt(object):
-    """
-    Interpolation Method - Python implementation of ScatteredInterpolation
-    """
+@dataclass
+class Interpolator:
+    """Interpolate scalar values onto the points of a mesh.
 
-    def __init__(self, points, values):
-        self.funcinterp = linterp(points, values)
-        self.funcnearest = nearest(points, values)
-
-    def __call__(self, *args):
-        z = self.funcinterp(*args)
-        chk = np.isnan(z)
-        if chk.any():
-            return np.where(chk, self.funcnearest(*args), z)
-        else:
-            return z
-
-
-def LocalSmoothing(x0, x1, smoothingLength):
-    f_dash = np.zeros(shape=(len(x1), 1), dtype=np.float64)
-    df_dash = np.zeros(shape=(len(x1), 3), dtype=np.float64)
-    [idx, dists] = calculate_points_within_distance(x0, x1, smoothingLength)
-
-    return [f_dash, df_dash]
-
-
-def rbfAssemble(x, phi, const, smooth):
-    xShape = x.shape
-    if len(xShape) == 2:
-        dim = xShape[0]
-        n = xShape[1]
-    else:
-        dim = 1
-        n = xShape[0]
-    A = np.zeros([n, n])
-    for i in range(n):
-        for j in range(i + 1):
-            r = np.linalg.norm(x[:, i] - x[:, j])
-            temp, _ = phi(r, const)
-            A[i, j] = temp
-            A[j, i] = temp
-        A[i, i] = A[i, i] - smooth
-    # Polynomial part
-    P = np.c_[np.ones(x.shape[1]), x.T]
-    A = np.c_[A, P]
-    A = np.r_[A, np.c_[P.T, np.zeros([dim + 1, dim + 1])]]
-    return A
-
-
-class rbfcreate:
-    def __init__(self, x, y, *args):
-        start_time = time.time()
-        nargin = 2 + len(args)
-
-        Names = [
-            "RBFFunction      ",
-            "RBFConstant      ",
-            "RBFSmooth        ",
-            "Stats            ",
-        ]
-        m = len(Names)
-        names = []
-        for i in range(m):
-            names.append(Names[i].lower())
-        # Check input arrays
-        xShape = x.shape
-        if len(xShape) == 2:
-            nXDim = xShape[0]
-            nXCount = xShape[1]
-        else:
-            nXDim = 1
-            nXCount = xShape[0]
-        yShape = y.shape
-        if len(yShape) == 2:
-            nYDim = yShape[0]
-            nYCount = yShape[1]
-        else:
-            nYDim = 1
-            nYCount = yShape[0]
-
-        if nXCount != nYCount:
-            sys.exit("x and y should have the same number of rows")
-
-        if nYDim != 1:
-            sys.exit("y should be n by 1 vector")
-
-        self.x = x
-        self.y = y
-
-        # Default values
-        self.RBFFunction = "linear"
-        self.RBFConstant = (
-            functools.reduce(
-                operator.mul, (np.amax(x.T, axis=0) - np.amin(x.T, axis=0)), 1
-            )
-            / nXCount
-        ) ** (
-            1 / nXDim
-        )  # approx. average distance between the nodes
-        self.RBFSmooth = 0
-        self.Stats = "off"
-
-        i = 0
-        if np.remainder(nargin - 2, 2) != 0:
-            sys.exit("Arguments must occur in name-value pairs.")
-
-        expectval = 0  # start expecting a name, not a value
-
-        while i < nargin - 2:
-            arg = args[i]
-
-            if not expectval:
-                if not isinstance(arg, str):
-                    sys.exit(
-                        "Expected argument " + i + " to be a string property name."
-                    )
-
-                lowArg = arg.lower()
-                j = []
-                for p in range(m):
-                    if names[p].startswith(lowArg):
-                        j.append(p)
-                if not j:  # if no matches
-                    sys.exit("Unrecognized property name " + arg)
-                elif len(j) > 1:  # if more than one match
-                    # Check for any exact matches (in case any names are subsets of others)
-                    k = []
-                    for p in range(m):
-                        if lowArg == names[p]:
-                            k.append(1)
-                    if len(k) == 1:
-                        j = k
-                    else:
-                        msg = (
-                            "Ambiguous property name "
-                            + arg
-                            + "("
-                            + Names[j[0]].replace(" ", "")
-                        )
-                        for p in range(1, len(k)):
-                            msg = msg + ", " + Names[j[p]].replace(" ", "")
-                        msg = msg + ")."
-                        sys.exit(msg)
-                expectval = 1  # we expect a value next
-            else:
-                if Names[j[0]].replace(" ", "") == "RBFFunction":
-                    self.RBFFunction = arg
-                elif Names[j[0]].replace(" ", "") == "RBFConstant":
-                    self.RBFConstant = arg
-                elif Names[j[0]].replace(" ", "") == "RBFSmooth":
-                    self.RBFSmooth = arg
-                elif Names[j[0]].replace(" ", "") == "Stats":
-                    self.stats = arg
-                expectval = 0
-            i += 1
-
-        if expectval:
-            sys.exit("Expected value for property " + arg)
-
-        # **************************************************************************
-        # Creating RBF Interpolatin
-        # **************************************************************************
-
-        if self.RBFFunction == "linear":
-            self.rbfphi = lambda r, const: (r, 1 / r)
-        elif self.RBFFunction == "cubic":
-            self.rbfphi = lambda r, const: (r * r * r, 3 * r)
-        elif self.RBFFunction == "multiquadric":
-            self.rbfphi = lambda r, const: (
-                np.sqrt(1 + r * r / (const * const)),
-                1 / (const * const * np.sqrt(1 + r * r / (const * const))),
-            )
-        elif self.RBFFunction == "thinplate":
-            self.rbfphi = lambda r, const: (
-                r * r * np.log(r + 1),
-                r / ((r + 1) + 2 * np.log(r + 1)),
-            )
-        elif self.RBFFunction == "gaussian":
-            self.rbfphi = lambda r, const: (
-                np.exp(-0.5 * r * r / (const * const)),
-                -np.exp(-0.5 * r * r / (const * const)) / (const * const),
-            )
-        else:
-            self.rbfphi = lambda r, const: (r, 1 / r)
-
-        phi = self.rbfphi
-
-        A = rbfAssemble(x, phi, self.RBFConstant, self.RBFSmooth)
-
-        b = np.r_[np.reshape(y, [nYCount, 1], order="F"), np.zeros([nXDim + 1, 1])]
-
-        # inverse
-        rbfcoeff = np.linalg.lstsq(A, b, rcond=None)[0]
-        self.rbfcoeff = rbfcoeff
-
-        if self.Stats == "on":
-            print(
-                len(y)
-                + "point RBF interpolation was created in "
-                + time.time()
-                - start_time
-                + " sec."
-            )
-
-
-def rbfinterp(x, options):
-
-    start_time = time.time()
-
-    phi = options.rbfphi
-    rbfconst = options.RBFConstant
-    nodes = options.x
-    rbfcoeff = options.rbfcoeff
-
-    dim, n = nodes.shape
-    dimPoints, nPoints = x.shape
-
-    if dim != dimPoints:
-        sys.exit(
-            "x should have the same number of rows as an array used to create RBF interpolation"
-        )
-
-    f = np.zeros([nPoints])
-    df = np.zeros([dimPoints, nPoints])
-
-    xDim, _ = x.shape
-
-    for i in range(nPoints):
-        ds = np.zeros([dimPoints, 1])
-        dx = (
-            np.matmul(np.reshape(x[:, i], [xDim, 1], order="F"), np.ones([1, n]))
-            - nodes
-        )
-        r = np.sqrt(np.sum(dx * dx, axis=0))
-
-        rbf, d_rbf = phi(r, rbfconst)
-
-        # First term is constant coefficient from monomial terms
-        s = rbfcoeff[n] + np.sum(rbfcoeff[0:n] * np.reshape(rbf, [n, 1], order="F"))
-        for k in range(dim):
-            s = s + rbfcoeff[k + n + 1] * x[k, i]  # add monomial terms
-            # Second term here are coefficients from monomial terms
-            ds[k] = (
-                np.sum(
-                    rbfcoeff[0:n]
-                    * np.reshape(dx[k, :], [n, 1], order="F")
-                    * np.reshape(d_rbf, [n, 1], order="F")
-                )
-                + rbfcoeff[k + n + 1]
-            )
-        f[i] = s
-        df[:, i] = np.reshape(ds, [1, dimPoints], order="F")
-
-    if options.Stats == "on":
-        print(
-            "Interpolation at "
-            + len(f)
-            + " points was computed in "
-            + time.time()
-            - start_time
-            + " sec."
-        )
-    return f, df
-
-
-def rbfcheck(options):
-    start_time = time.time()
-
-    nodes = options.x
-    y = options.y
-
-    s, _ = rbfinterp(nodes, options)
-
-    print("RBF Check")
-    print("max|y - yi| = " + str(np.max(abs(s - y))))
-
-    if options.Stats == "on":
-        print(len(y) + " points were checked in " + time.time() - start_time + " sec.")
-
-
-def rbf_scipy(x0, d0, x1, rbfConstant):
-
-    x = x0[:, 0]
-    y = x0[:, 1]
-    z = x0[:, 2]
-    d = d0
-
-    F = Rbf(x, y, z, d, function="multiquadric", smooth=rbfConstant)
-    d1 = F(x1[:, 0], x1[:, 1], x1[:, 2])
-
-    return d1
-
-
-class OpenEPDataInterpolator:
-    """
-    Creates objects for performing spatial interpolation for OpenEP data
+    Args:
+        points (np.ndarray): (N,3) array of coordinates for which we know values
+            of the scalar field
+        field (np.ndarray): array of size N of scalar values
+        method (callable): method to use for interpolation. The default is
+            scipy.interpolate.RBFInterpolator.
+        method_kws (dict): dictionary of keyword arguments to pass to `method`
+            when creating the interpolator
     """
 
-    def __init__(self, method, distanceThreshold, rbfConstant):
-        self.method = method
-        self.distanceThreshold = distanceThreshold
-        self.rbfConstant = rbfConstant
+    points: np.ndarray
+    field: np.ndarray
+    method: callable = scipy.interpolate.RBFInterpolator
+    method_kws: dict = None
 
-    def interpolate(self, x0, d0, x1, *args):
-        self.x0 = x0
-        self.d0 = d0
-        self.x1 = x1
+    def __post_init__(self):
+        """Create the interpolator"""
 
-        if self.method == "scatteredinterpolant":
-            F = LinearNDInterpolatorExt(
-                points=(self.x0[:, 0], self.x0[:, 1], self.x0[:, 2]), values=self.d0
-            )
-            d1 = np.array(F(self.x1[:, 0], self.x1[:, 1], self.x1[:, 2])).reshape(
-                self.x1.shape[0], 1
-            )
+        default_rbf_kws = {
+            "smoothing": 4,
+            "epsilon": 0.5,
+            "degree": 1,
+        }
 
-        elif self.method == "rbf":
-            op = rbfcreate(
-                self.x0.T,
-                self.d0.T,
-                "RBFFunction",
-                "multiquadric",
-                "RBFConstant",
-                self.rbfConstant,
-            )
-            rbfcheck(op)
-            out = rbfinterp(self.x1.T, op)
-            d1 = np.array(out[0]).reshape(self.x1.shape[0], 1)
-
-        elif self.method == "localSmoothing":
-            pass
-
+        if self.method_kws is not None:
+            if self.method is scipy.interpolate.RBFInterpolator:
+                self.method_kws = {**default_rbf_kws, **self.method_kws}
+        elif self.method is scipy.interpolate.RBFInterpolator:
+            self.method_kws =  default_rbf_kws
         else:
-            print("Interpolation Method not Found")
+            raise ValueError("method_kws must be specified if scipy.interpolate.RBFInterpolator is not used")
 
-        # Workout if there are any points on the surface that are < 0
-        d1[d1 < 0] = 0
-
-        #  work out which points on the surface are too far away from real data
-        # Remove any interpolated values which are outwith the fill threshold
-
-        from sklearn.neighbors import NearestNeighbors
-
-        neigh = NearestNeighbors(
-            n_neighbors=1,
-            radius=1,
-            algorithm="auto",
-            leaf_size=30,
-            metric="minkowski",
-            p=2,
+        self.interpolate = self.method(
+            self.points,
+            self.field,
+            **self.method_kws,
         )
 
-        neigh.fit(self.x0)
 
-        id = neigh.kneighbors(self.x1, return_distance=False)
-        cPts = self.x0[id]
-        cPts = np.array(cPts[:, 0])
+    def __call__(self, surface_points, max_distance=None):
+        """Interpolate the scalar field onto a new set of coordinates
 
-        # Is this correct? It's only calculating row-wise distance, not all combinaions
-        d = calculate_distance(cPts, self.x1).diagonal()[:, np.newaxis]
+        Args:
+            surface_points (ndarray): Mx3 array of points onto which the field will be
+                interpolated
+            max_distance (float, optional): Surface points further than this distance from any of the
+                original points will not be used in the interpolation. Instead, their scalar field will
+                be set to NaN. Defaults to None, in which case all surface points will 
 
-        thresholdDistance = np.zeros(shape=d.shape, dtype=np.bool)
-        thresholdDistance[d > self.distanceThreshold] = 1
-        d1[thresholdDistance] = "nan"
-        d1 = d1.flatten()
+        Returns:
+            interpolated_field (ndarray): Scalar field interpolated onto the new points.
+        """
 
-        return d1
+        interpolated_field = self.interpolate(surface_points)
+
+        if max_distance is not None:
+            within_distance = calculate_points_within_distance(
+                self.points,
+                surface_points,
+                max_distance=max_distance,
+                return_distances=False
+            )
+            within_distance = np.any(within_distance, axis=1)
+            interpolated_field[~within_distance] = np.NaN
+
+        return interpolated_field
 
 
 def get_voltage_electroanatomic(mesh_case):
