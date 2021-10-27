6f7a7dec2e349060d386f353ad697ca937ca1589
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-27 19:31:22 +0100

    Add tests for case_routines

---
+++ tests/test_case_routines.py
@@ -0,0 +1,206 @@
+# OpenEP
+# Copyright (c) 2021 OpenEP Collaborators
+#
+# This file is part of OpenEP.
+#
+# OpenEP is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# OpenEP is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program (LICENSE.txt).  If not, see <http://www.gnu.org/licenses/>
+
+import pytest
+from numpy.testing import assert_allclose, assert_array_equal
+
+import numpy as np
+
+from openep.case.case_routines import (
+    _get_reference_annotation,
+    _get_window_of_interest,
+    get_mapping_points_within_woi,
+    get_electrograms_at_points,
+    get_woi_times,
+    calculate_voltage_from_electrograms
+)
+
+
+@pytest.fixture()
+def mock_case(mocker):
+    
+    case = mocker.patch('openep.data_structures.case.Case')
+
+    case.electric.annotations.reference_activation_time = np.full(10, fill_value=5, dtype=int)
+    case.electric.annotations.local_activation_time = np.arange(5, 25, 2)
+    case.electric.annotations.window_of_interest = np.full((10, 2), fill_value=5)
+    case.electric.annotations.window_of_interest[:, 1] += 10
+
+    case.electric.bipolar_egm.egm = np.repeat(np.arange(20)[np.newaxis, :], 10, axis=0)
+    case.electric.internal_names = np.arange(10).astype(str)
+    
+    return case
+
+
+def test_get_reference_annotation(mock_case):
+
+    reference_annotations = _get_reference_annotation(mock_case, indices=None)
+
+    assert_allclose(
+        mock_case.electric.annotations.reference_activation_time,
+        reference_annotations,
+    )
+
+
+@pytest.mark.parametrize('indices', [1, [0, 1, 5]])
+def test_get_reference_annotation_indices(mock_case, indices):
+
+    reference_annotations = _get_reference_annotation(mock_case, indices=indices)
+
+    assert_allclose(
+        mock_case.electric.annotations.reference_activation_time[indices],
+        reference_annotations,
+    )
+
+
+def test_get_window_of_interest(mock_case):
+
+    window_of_interest = _get_window_of_interest(mock_case, indices=None)
+
+    assert_allclose(
+        mock_case.electric.annotations.window_of_interest,
+        window_of_interest,
+    )
+
+
+@pytest.mark.parametrize('indices', [1, [0, 1, 5]])
+def test_get_window_of_interest_indices(mock_case, indices):
+
+    window_of_interest = _get_window_of_interest(mock_case, indices=indices)
+
+    assert_allclose(
+        mock_case.electric.annotations.window_of_interest[indices],
+        window_of_interest,
+    )
+
+
+def test_get_mapping_points_within_woi(mock_case):
+
+    within_woi = get_mapping_points_within_woi(mock_case)
+
+    assert within_woi.all()
+
+
+def test_get_mapping_points_within_woi_indices(mock_case):
+
+    within_woi = get_mapping_points_within_woi(mock_case, indices=[0, 1, 5])
+
+    assert within_woi.size == 3
+    assert within_woi.all()
+
+
+def test_get_mapping_points_within_woi_no_buffer(mock_case):
+
+    within_woi = get_mapping_points_within_woi(mock_case, indices=[3, 4, 5, 6, 7], buffer=0)
+    outside_woi = get_mapping_points_within_woi(mock_case, indices=[0, 1, 2, 8, 9], buffer=0)
+
+    assert within_woi.size == 5
+    assert within_woi.all()
+
+    assert outside_woi.size == 5
+    assert not outside_woi.any()
+
+
+def test_get_electrograms_at_points(mock_case):
+
+    electrograms, names, lat = get_electrograms_at_points(mock_case, within_woi=False)
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm, electrograms)
+    assert_array_equal(mock_case.electric.internal_names, names)
+    assert_allclose(mock_case.electric.annotations.local_activation_time, lat)
+
+def test_get_electrograms_at_points_within_woi(mock_case):
+
+    electrograms, names, lat = get_electrograms_at_points(mock_case, within_woi=True, buffer=5)
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm, electrograms)
+    assert_array_equal(mock_case.electric.internal_names, names)
+    assert_allclose(mock_case.electric.annotations.local_activation_time, lat)
+
+
+def test_get_electrograms_at_points_within_woi_no_buffer(mock_case):
+
+    electrograms, names, lat = get_electrograms_at_points(mock_case, within_woi=True, buffer=0)
+    within_woi = [3, 4, 5, 6, 7]
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm[within_woi], electrograms)
+    assert_array_equal(mock_case.electric.internal_names[within_woi], names)
+    assert_allclose(mock_case.electric.annotations.local_activation_time[within_woi], lat)
+
+
+def test_get_electrograms_at_points_indices(mock_case):
+
+    indices = [0, 1, 5]
+    electrograms, names, lat = get_electrograms_at_points(mock_case, within_woi=False, indices=indices)
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm[indices], electrograms)
+    assert_array_equal(mock_case.electric.internal_names[indices], names)
+    assert_allclose(mock_case.electric.annotations.local_activation_time[indices], lat)
+
+
+def test_get_electrograms_at_points_no_lat(mock_case):
+
+    electrograms, names = get_electrograms_at_points(mock_case, within_woi=False, return_lat=False)
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm, electrograms)
+    assert_array_equal(mock_case.electric.internal_names, names)
+
+
+def test_get_electrograms_at_points_no_names(mock_case):
+
+    electrograms, lat = get_electrograms_at_points(mock_case, within_woi=False, return_names=False)
+
+    assert_allclose(mock_case.electric.bipolar_egm.egm, electrograms)
+    assert_allclose(mock_case.electric.annotations.local_activation_time, lat)
+
+
+def test_get_electrograms_at_points_no_lat_or_names(mock_case):
+
+    electrograms = get_electrograms_at_points(mock_case, within_woi=False, return_lat=False, return_names=False)
+    assert_allclose(mock_case.electric.bipolar_egm.egm, electrograms)
+
+
+def test_get_woi_times(mock_case):
+    
+    times = get_woi_times(mock_case)
+    assert_allclose(np.arange(20), times)
+
+
+def test_get_woi_times_no_buffer(mock_case):
+    
+    times = get_woi_times(mock_case, buffer=0)
+    assert_allclose(np.arange(10, 20), times)
+
+def test_get_woi_times_no_buffer_relative(mock_case):
+    
+    times = get_woi_times(mock_case, buffer=0, relative=True)
+    assert_allclose(np.arange(5, 15), times)
+
+
+def test_calculate_voltage_from_electrograms(mock_case):
+
+    amplitudes = calculate_voltage_from_electrograms(mock_case)
+    assert_allclose(19, amplitudes)
+
+
+def test_calculate_voltage_from_electrograms_no_buffer(mock_case):
+
+    amplitudes = calculate_voltage_from_electrograms(mock_case, buffer=0)
+    assert_allclose(9, amplitudes)
+
+
