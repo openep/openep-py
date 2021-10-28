0f2d95b8bd4108cb0b648a2c797dfb8ad634f5a8
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-28 16:43:59 +0100

    Setup basic outline of docs

---
+++ docs/api/api.rst
@@ -0,0 +1,10 @@
+.. _API:
+
+API
+===
+
+See the following pages for the full API of the classes and functions:
+
+.. toctree::
+   :maxdepth: 1
+
--- docs/conf.py
+++ docs/conf.py
@@ -15,12 +15,14 @@ import sys
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
-
 # -- Project information -----------------------------------------------------
 
 project = 'OpenEP'
-copyright = '2021, Steven Williams, Jerin Rajan, Eric Kerfoot (c) 2021 KCL'
-author = 'Steven Williams, Jerin Rajan, Eric Kerfoot (c) 2021 KCL'
+copyright = '2021, Steven Williams, Jerin Rajan, Eric Kerfoot, Paul Smith (c) 2021 KCL'
+author = 'Steven Williams, Jerin Rajan, Eric Kerfoot, Paul Smith (c) 2021 KCL'
+
+# The full version, including alpha/beta/rc tags
+version = release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,7 +31,16 @@ author = 'Steven Williams, Jerin Rajan, Eric Kerfoot (c) 2021 KCL'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc"
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -40,15 +51,33 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+autodoc_typehints = 'signature'
+autodoc_docstring_signature = True
+autoclass_content = 'both'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_use_smartypants = True
+html_last_updated_fmt = '%b %d, %Y'
+html_split_index = False
+html_sidebars = {
+   '**': ['searchbox.html', 'globaltoc.html', 'sourcelink.html'],
+}
+html_short_title = '%s-%s' % (project, version)
+
+napoleon_use_ivar = True
+napoleon_use_rtype = False
+napoleon_use_param = False
+
