b36d98bec552ee5316026bbcf33ef34e4e8c461c
Paul Smith <paul.smith@kcl.ac.uk>
2021-10-28 13:00:38 +0100

    Add tests for FreeBoundary

--- examples/openep_freeboundaries.py
+++ examples/openep_freeboundaries.py
@@ -1,11 +1,13 @@
+import pyvista
 import openep
-
+from openep._datasets.simple_meshes import TRIANGLES
 
 filename = "/Users/paul/github/openep-py/examples/data/new_dataset_2.mat"
 
 case = openep.load_case(filename)
 mesh = case.create_mesh()
 
+mesh = pyvista.read(TRIANGLES)
 free_boundaries = openep.mesh.get_free_boundaries(mesh)
 print(f"Perimeter lengths: {free_boundaries.calculate_lengths()}")
 print(f"Cross-sectional areas: {free_boundaries.calculate_areas()}")
--- tests/test_mesh_routines.py
+++ tests/test_mesh_routines.py
@@ -3,10 +3,16 @@ from numpy.testing import assert_allclose
 
 import numpy as np
 import pyvista
+import trimesh
 
 from openep.mesh.mesh_routines import (
-    calculate_field_area, calculate_mesh_volume,
-    calculate_vertex_distance, calculate_vertex_path
+    _create_trimesh,
+    FreeBoundary,
+    get_free_boundaries,
+    calculate_field_area,
+    calculate_mesh_volume,
+    calculate_vertex_distance,
+    calculate_vertex_path,
 )
 from openep._datasets.simple_meshes import (
     CUBE, SPHERE, BROKEN_SPHERE, TRIANGLES
@@ -49,6 +55,53 @@ def broken_sphere():
 @pytest.fixture(scope='module')
 def triangles():
     return pyvista.read(TRIANGLES)
+
+@pytest.fixture(scope='module')
+def free_boundaries(triangles):
+
+    return get_free_boundaries(triangles)
+
+def test_create_trimesh(cube):
+
+    faces = cube.faces.reshape(-1, 4)[:, 1:]
+    trimesh_cube = _create_trimesh(cube)
+
+    assert isinstance(trimesh_cube, trimesh.Trimesh)
+    assert_allclose(cube.points, trimesh_cube.vertices)
+    assert_allclose(faces, trimesh_cube.faces)
+
+def test_get_free_boundaries(triangles, free_boundaries):
+
+    # The first point of the triangles mesh is the centre of a square, and so not on a free boundary
+    assert 0 not in free_boundaries.original_lines
+    assert not np.any(np.all(triangles.points[0] == free_boundaries.points, axis=1))
+
+    # All other points are part of free boundaries
+    for point in triangles.points[1:]:
+        assert np.any(np.all(point == free_boundaries.points, axis=1))
+
+    assert 2 == free_boundaries.n_boundaries
+    assert_allclose([5, 4], free_boundaries.n_points_per_boundary)
+
+def test_FreeBoundary_calculate_areas(free_boundaries):
+
+    # To calculate areas, we need to construct the boundary meshes based on the boundary points
+    assert free_boundaries._boundary_meshes is None
+    areas = free_boundaries.calculate_areas()
+    assert_allclose([1, 0.5], areas)
+
+    # Upon requestning the areas a second time, the boundary meshes should already be present
+    new_areas = free_boundaries.calculate_areas()
+    assert free_boundaries._boundary_meshes is not None
+    assert_allclose(areas, new_areas)
+
+def test_FreeBoundary_calculate_lengths(free_boundaries):
+
+    square_perimeter = 4
+    triangle_perimeter = 2 + np.sqrt(2)
+
+    areas = free_boundaries.calculate_lengths()
+    assert_allclose([square_perimeter, triangle_perimeter], areas)
 
 
 def test_calculate_mesh_volume(sphere):
