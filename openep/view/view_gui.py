43c2665eaa784060f66c1fd810033564d0033c0a
Paul Smith <paul.smith@kcl.ac.uk>
2021-11-05 10:43:26 +0000

    Tidy up initialisation of the gui.
    Add basic docstrings. Used separate functions for initialising each of the dockable widgets. Create the menubar using qmainwindow rather than manually with qwidgets.

--- openep/view/view_gui.py
+++ openep/view/view_gui.py
@@ -74,54 +74,73 @@ class OpenEpGUI(QtWidgets.QMainWindow):
     def __init__(self):
 
         super().__init__()
-        self._initUI()
-        self._add_menu_bar()
-        
-        self.egm_point = np.array([0], dtype=int)
 
-        # initial bipolar voltage colourbar limits
+        self._init_data()
+        self._init_ui()
+        self._add_menu_bar()
+        self._create_plotter_1_dock()
+        self._create_plotter_2_dock()
+        self._create_canvas_3_dock()
+        self._create_canvas_4_dock()
+        self._add_dock_widgets()
+    
+    def _init_data(self):
+        """
+        Set default values for variables that can later be set by the user.
+        """
+        
+        # Display only the first electrogram
+        self.egm_point = np.array([0], dtype=int)
+        
+        # initial bipolar voltage colourbar limits
         self._initial_lower_limit_1 = 0
         self._initial_upper_limit_1 = 2
-        
-        # initial LAT colourbar limits
+
+        # initial LAT colourbar limits
         self._initial_lower_limit_2 = 0
         self._initial_upper_limit_2 = 200
 
-        self._add_dock_widgets()
+    def _init_ui(self):
+        """
+        Initialise the GUI.
 
-    def _initUI(self):
+        Lots of the layout is handled automatically by QtWidgets.QMainWindow.
+        """
 
         self.setWindowTitle("OpenEP: The open-source solution for electrophysiology data analysis")
-        self.mainLayout = QtWidgets.QVBoxLayout()
-        self.menulayout = QtWidgets.QGridLayout()
-        self.buttonLayout = QtWidgets.QHBoxLayout()
     
     def _add_menu_bar(self):
+        """Add a menu bar to the GUI."""
 
-        menubar = QtWidgets.QMenuBar()
-        self.menulayout.addWidget(menubar, 0, 0)
+        menubar = self.menuBar()
         file_menu = menubar.addMenu("File")
+        menubar.setNativeMenuBar(True)
         
         self.load_case_act = QtWidgets.QAction("Open File...")
         self.load_case_act.triggered.connect(self.load_data)
         file_menu.addAction(self.load_case_act)
         file_menu.addSeparator()
+
+    def _create_plotter_1_dock(self):
+        """
+        Create a dockable pyvista-qt plotter for rendering 3D maps.
         
-    def _add_dock_widgets(self):
+        This can be used for projecting scalar fields onto the 3D surface.
+        Currently, only bipolar voltage is supported.
+        """
 
-        dock_1 = DockWidget("Voltage")
-        dock_2 = DockWidget("LAT")
-        dock_3 = DockWidget("EGMs")
-        dock_4 = DockWidget("Analysis")
-
-        # Plotter 1 (defaults to bipolar voltage)
+        # TODO: add support for changing the scalar field
+        
+        # Plotter 1 defaults to bipolar voltage
+        self.dock_1 = DockWidget("Voltage")
+        
         frame_1 = QtWidgets.QFrame()
         plotter_1 = QtInteractor(frame_1)
         plotter_1.background_color = 'white'
         plotter_1.setMinimumSize(QtCore.QSize(50, 50))
         self.plotter_1 = plotter_1
-        
-        # Add thresholds to plotter 1
+
+        # Add thresholds to plotter 1
         self.plotter_1.limitLayout = QtWidgets.QFormLayout(self)
         
         self.lower_limit_1 = QtWidgets.QLineEdit("Lower threshold", self.plotter_1)
@@ -142,14 +161,28 @@ class OpenEpGUI(QtWidgets.QMainWindow):
         button_set_thresholds_1.clicked.connect(self.update_colourbar_limits_1)
         self.plotter_1.limitLayout.addRow(button_set_thresholds_1)
         
-        # Plotter 2 (defaults to LAT)
+        self.dock_1.setWidget(self.plotter_1)
+
+    def _create_plotter_2_dock(self):
+        """
+        Create a second dockable pyvista-qt plotter for rendering 3D maps.
+        
+        This can be used for displaying alternative scalar fields to `plotter_1`.
+        Currently, only local activation time is supported.
+        """
+
+        # TODO: add support for changing the scalar field
+
+        # Plotter 2 defaults to local activation time
+        self.dock_2 = DockWidget("LAT")
+
         frame_2 = QtWidgets.QFrame()
-        plotter_2= QtInteractor(frame_2)
+        plotter_2 = QtInteractor(frame_2)
         plotter_2.background_color = 'white'
         plotter_2.setMinimumSize(QtCore.QSize(50, 50))
         self.plotter_2 = plotter_2
 
-        # Add thresholds to plotter 2
+        # Add thresholds to plotter 2
         self.plotter_2.limitLayout = QtWidgets.QFormLayout(self)        
         
         self.lower_limit_2 = QtWidgets.QLineEdit("Lower threshold", self.plotter_2)
@@ -169,9 +202,15 @@ class OpenEpGUI(QtWidgets.QMainWindow):
         button_set_thresholds_2.setGeometry(10, 10, 180, 40)
         button_set_thresholds_2.clicked.connect(self.update_colourbar_limits_2)
         self.plotter_2.limitLayout.addRow(button_set_thresholds_2)
+        
+        self.dock_2.setWidget(self.plotter_2)
 
-        # MPL canvases
-        # Canvas 1 (EGMs)
+    def _create_canvas_3_dock(self):
+        """Create a dockable widget for plotting interactive electrograms with matplotlib"""
+
+        # Canvas 3 is for the electrograms
+        self.dock_3 = DockWidget("EGMs")
+
         self.figure_3, self.axis_3 = plt.subplots(ncols=1, nrows=1)
         self.figure_3.set_facecolor("white")
         self.canvas_3 = FigureCanvas(self.figure_3)
@@ -193,7 +232,7 @@ class OpenEpGUI(QtWidgets.QMainWindow):
         egm_layout.addRow(button_egm_select)
         
         # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar = NavigationToolbar(self.canvas_3, dock_3)
+        toolbar = NavigationToolbar(self.canvas_3, self.dock_3)
         canvas_layout = QtWidgets.QVBoxLayout()
         canvas_layout.addWidget(self.canvas_3)
         canvas_layout.addWidget(toolbar)
@@ -203,29 +242,38 @@ class OpenEpGUI(QtWidgets.QMainWindow):
         canvas_widget.setLayout(canvas_layout)
         canvas_widget.setStyleSheet("border-width: 0px; border: 0px; background-color:white;")
 
-        # Other analysis (e.g. histogram of voltages)
+        self.dock_3.setWidget(canvas_widget)
+
+    def _create_canvas_4_dock(self):
+        """Create a dockable widget for other matplotlib plots."""
+
+        # TODO: Add matplotlib canvas for plotting results from other analyses
+
+        # Canvas 4 is for plotting other analyses (e.g. histogram of voltages)
+        self.dock_4 = DockWidget("Analysis")
+
         content4 = QtWidgets.QWidget()
         content4.setStyleSheet("background-color:white;")
         content4.setMinimumSize(QtCore.QSize(50, 50))
 
-        dock_1.setWidget(self.plotter_1)
-        dock_2.setWidget(self.plotter_2)
-        dock_3.setWidget(canvas_widget)
-        dock_4.setWidget(content4)
-        
-        self.addDockWidget(Qt.LeftDockWidgetArea, dock_1)
-        self.addDockWidget(Qt.LeftDockWidgetArea, dock_2)
-        self.tabifyDockWidget(dock_1, dock_2)
-        self.addDockWidget(Qt.RightDockWidgetArea, dock_3)
-        self.addDockWidget(Qt.LeftDockWidgetArea, dock_4)
-        self.tabifyDockWidget(dock_3, dock_4)
+        self.dock_4.setWidget(content4)
+
+    def _add_dock_widgets(self):
+        """Add dockable widgets to the main window."""
+
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_1)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_2)
+        self.tabifyDockWidget(self.dock_1, self.dock_2)
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock_3)
+        self.addDockWidget(Qt.LeftDockWidgetArea, self.dock_4)
+        self.tabifyDockWidget(self.dock_3, self.dock_4)
 
         self.setDockOptions(self.GroupedDragging | self.AllowTabbedDocks | self.AllowNestedDocks)
         self.setTabPosition(Qt.AllDockWidgetAreas, QtWidgets.QTabWidget.North)
 
-        for dock in [dock_1, dock_2, dock_3, dock_4]:
+        for dock in [self.dock_1, self.dock_2, self.dock_3, self.dock_4]:
             dock.setAllowedAreas(Qt.AllDockWidgetAreas)
-        
+
     def load_data(self):
         print("Please wait: Loading Data ... ")
 
@@ -338,9 +386,17 @@ class OpenEpGUI(QtWidgets.QMainWindow):
         self.canvas_3.draw()
 
 
-if __name__ == "__main__":
+def main():
 
+    # Create an instance of Qapplication
     app = QtWidgets.QApplication(sys.argv)
-    main = OpenEpGUI()
-    main.showMaximized()
+
+    # Create an instance of GUI
+    window = OpenEpGUI()
+    window.showMaximized()
+
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
