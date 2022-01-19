d2439be8184d73ba4164640e0a9a8adf32c57828
Paul Smith <paul.smith@kcl.ac.uk>
2022-01-19 13:55:25 +0000

    Set correct version info

--- setup.py
+++ setup.py
@@ -5,8 +5,9 @@ from pkg_resources import parse_requirements
 source_dir = os.path.abspath(os.path.dirname(__file__))
 
 # read the version and other strings from _version.py
+version_info = {}
 with open(os.path.join(source_dir, "openep/_version.py")) as o:
-    exec(o.read())
+    exec(o.read(), version_info)
 
 # read install requirements from requirements.txt
 with open(os.path.join(source_dir, "requirements.txt")) as o:
@@ -14,7 +15,7 @@ with open(os.path.join(source_dir, "requirements.txt")) as o:
 
 setup(
     name="OpenEp",
-    version="0.1",
+    version=version_info['__version__'],
     description="Open Source solution for electrophysiology data analysis",
     author="Steven Williams",
     author_email="steven.williams@ed.ac.uk",
