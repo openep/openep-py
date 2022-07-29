650acffc4a8852d341a906af092cce275473bb30
Paul Smith <paul.smith@kcl.ac.uk>
2022-07-29 20:42:12 +0100

    Remove docs and tests for broken function get_woi_times

--- openep/case/__init__.py
+++ openep/case/__init__.py
@@ -2,7 +2,6 @@ __all__ = ['case_routines']
 
 from .case_routines import (
     get_mapping_points_within_woi,
-    get_woi_times,
     get_electrograms_at_points,
     calculate_voltage_from_electrograms,
     calculate_distance,
