b9b9c39cbcf750530055a47199959f2ba9932b55
Paul Smith <paul.smith@kcl.ac.uk>
2022-08-09 12:33:33 +0100

    Add test for case.remove_unreferenced_points

--- openep/_datasets/meshes.py
+++ openep/_datasets/meshes.py
@@ -5,12 +5,19 @@ Meshes created for testing openep
 MESH_2 was created from the OpenEP DATASET_2. The largest surface was extracted
 and the surface then smoothed.
 
+MESH_2_dense was created from the OpenEP DATASET_2. This mesh has had all nodes
+not referenced in the triangulation removed.
+
 """
 __all__ = [
-    "MESH_2",
+    "MESH_2_SMOOTH",
+    "MESH_2_DENSE",
 ]
 
 from pkg_resources import resource_filename
 
-MESH_2 = resource_filename(__name__,
+MESH_2_SMOOTH = resource_filename(__name__,
                            "VTK/openep_mesh_2_smooth.vtk")
+
+MESH_2_DENSE = resource_filename(__name__,
+                           "VTK/openep_mesh_2_dense.vtk")
